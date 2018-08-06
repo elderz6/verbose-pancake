f68124cbfde81bbd716d198527fe5880c34ab50b
elderz6 <Padrao@NTHTCPS001755.hildebra.intra>
2018-08-06 15:00:35 -0300

    fixed reducer bug

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-class HomePage extends React.Component {
+class HomePage extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      isAuthenticated: false
+    };
+  }
   render () {
     return(
       <div>
         <h1> Home Page </h1>
-        <Link to='/login'> Login </Link>
+      { this.props.isAuthenticated
+        ? <button> Logout</button>
+        :  <Link to='/login'> Login </Link>}
       </div>
     );
   }
 }
 
-export default HomePage;
+HomePage.propTypes =
+{
+  isAuthenticated: PropTypes.bool.isRequired
+}
+
+const mapStatetoProps = state =>
+{
+  return{
+    isAuthenticated: !!state.user.token
+  }
+}
+
+export default connect(mapStatetoProps)(HomePage);
